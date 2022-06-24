bd44bfa1ae097e414762cbd41d483d7164982e1e
Damian-luis <damian.luis.porta@gmail.com>
2022-06-24 00:37:48 -0300

    dying

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import './App.css';
-
+import react from 'react';
+import { useState } from 'react'
 function App() {
+
+
+  const [estado,setEstado]=useState("");
+
+  function handlerState(e){
+    var a=estado
+    a=estado+e.target.value
+    parseInt(a)
+      setEstado(a);
+  }
+  function handlerResult(e){
+    
+  }
+  function handlerReset(){
+    setEstado()
+  }
   return (
     <div className="App">
       <div className="body">
         <div className="calculadora">
-          <div className="pantalla"></div>
+          <div className="pantalla">
+            {estado?<h4>{estado}</h4>:<h4>_</h4>}
+          </div>
 
 
 
 
           <div className="botones">
             <div className="numeros">
-                <div className="number"><span>1</span></div>
-                <div className="number"><span>2</span></div>
-                <div className="number"><span>3</span></div>
-                <div className="number"><span>4</span></div>
-                <div className="number"><span>5</span></div>
-                <div className="number"><span>6</span></div>
-                <div className="number"><span>7</span></div>
-                <div className="number"><span>8</span></div>
-                <div className="number"><span>9</span></div>
-                <div className="number"><span>0</span></div>
+                <button onClick={handlerState} value="1" className="number"><span>1</span></button>
+                <button onClick={handlerState} value="2" className="number"><span>2</span></button>
+                <button onClick={handlerState} value="3" className="number"><span>3</span></button>
+                <button onClick={handlerState} value="4" className="number"><span>4</span></button>
+                <button onClick={handlerState} value="5" className="number"><span>5</span></button>
+                <button onClick={handlerState} value="6" className="number"><span>6</span></button>
+                <button onClick={handlerState} value="7" className="number"><span>7</span></button>
+                <button onClick={handlerState} value="8" className="number"><span>8</span></button>
+                <button onClick={handlerState} value="9" className="number"><span>9</span></button>
+                <button onClick={handlerReset} value="reset" className="number orange"><span>C</span></button>
+                <button onClick={handlerState} value="0" className="number"><span>0</span></button>
+                <button onClick={handlerResult} value="solve" className="number orange"><span>=</span></button>
             </div>
             <div className="simbolos">
-              <div className="simbol"><span>+</span></div>
-              <div className="simbol"><span>-</span></div>
-              <div className="simbol"><span>*</span></div>
-              <div className="simbol"><span>/</span></div>
+              <button onClick={handlerState} value="+" className="simbol"><span>+</span></button>
+              <button onClick={handlerState} value="-" className="simbol"><span>-</span></button>
+              <button onClick={handlerState} value="*" className="simbol"><span>*</span></button>
+              <button onClick={handlerState} value="/" className="simbol"><span>/</span></button>
               </div>  
           </div>
 
